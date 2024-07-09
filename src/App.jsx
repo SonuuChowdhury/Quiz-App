611d3cf036a162f5277fa027112c4afd2fa30e12
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-07-09 15:32:42 +0530

    Reset Button Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   // StateVariable defined as questions options and correct
   // correct shows the correct option of the question as 0 1 2 3
   // check : 0=> not answered
-  //         1=> answered but incorrect 
-  //         2=? answered and correct 
+  //         1=> answered but incorrect
+  //         2=? answered and correct
   const [question, setQuestion] = useState([
     {
       mainQuestion: "Who Was the first PM of India?",
@@ -17,19 +17,19 @@ function App() {
         "Indira Gandhi",
       ],
       correct: 0,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "What is the capital of France?",
       options: ["Berlin", "Madrid", "Paris", "Rome"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "Which planet is known as the Red Planet?",
       options: ["Earth", "Mars", "Jupiter", "Saturn"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "Who wrote 'Romeo and Juliet'?",
@@ -38,28 +38,27 @@ function App() {
         "Charles Dickens",
         "Mark Twain",
         "Jane Austen",
-        
       ],
       correct: 0,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "What is the largest mammal in the world?",
       options: ["Elephant", "Blue Whale", "Giraffe", "Polar Bear"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "Which element has the chemical symbol 'O'?",
       options: ["Oxygen", "Gold", "Osmium", "Oganesson"],
       correct: 0,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "What is the tallest mountain in the world?",
       options: ["K2", "Mount Everest", "Kangchenjunga", "Lhotse"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "Who painted the Mona Lisa?",
@@ -70,25 +69,25 @@ function App() {
         "Claude Monet",
       ],
       correct: 0,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "What is the smallest country in the world by land area?",
       options: ["Monaco", "Vatican City", "Nauru", "San Marino"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "In which year did the Titanic sink?",
       options: ["1905", "1912", "1920", "1930"],
       correct: 1,
-      check:0
+      check: 0,
     },
     {
       mainQuestion: "Which language is primarily spoken in Brazil?",
       options: ["Spanish", "Portuguese", "French", "English"],
       correct: 1,
-      check:0
+      check: 0,
     },
   ]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -96,10 +95,10 @@ function App() {
     content: "Start",
     flag: true,
   });
-  const [NoOfQuestionAnswered,SetnoOfQuestionAnswered]=useState(0);
-  const [NoOfQuestionCorrect,SetnoOfQuestionCorrect]=useState(0);
-  const [NoOfQuestionInCorrect,SetnoOfQuestionInCorrect]=useState(0);
-  const [StatusBar, SetStatusBar]=useState("Enter Your Choice")
+  const [NoOfQuestionAnswered, SetnoOfQuestionAnswered] = useState(0);
+  const [NoOfQuestionCorrect, SetnoOfQuestionCorrect] = useState(0);
+  const [NoOfQuestionInCorrect, SetnoOfQuestionInCorrect] = useState(0);
+  const [StatusBar, SetStatusBar] = useState("Enter Your Choice");
 
   // main button content updater
   function MainButtonContentUpdate() {
@@ -111,6 +110,7 @@ function App() {
         });
       } else {
         if (currentQuestion == 0) {
+          alert("Question Reset Done");
           SetMainButtonContent({
             content: "Start",
             flag: true,
@@ -119,20 +119,22 @@ function App() {
       }
     }
   }
-
   // Start button handeller
   function StartButtonHandeller() {
+    setQuestionFunctionAll(0);
     setCurrentQuestion(0);
+    SetnoOfQuestionAnswered(0);
+    SetnoOfQuestionCorrect(0);
+    SetnoOfQuestionInCorrect(0);
     MainButtonContentUpdate;
   }
 
   // Next Button handeler
   function NextButtonHandeller() {
-    SetStatusBar("Enter Your Choice")
-    if (currentQuestion==question.length-1) {
-      alert("No more Questions") 
-    }
-    else{
+    SetStatusBar("Enter Your Choice");
+    if (currentQuestion == question.length - 1) {
+      alert("No more Questions");
+    } else {
       setCurrentQuestion(currentQuestion + 1);
     }
     MainButtonContentUpdate();
@@ -140,77 +142,78 @@ function App() {
 
   //PreviousButton handeler
   function PreviousButtonHandeller() {
-    if (currentQuestion==0) {
-      alert("No previous Questions")
-    }
-    else{
+    if (currentQuestion == 0) {
+      alert("No previous Questions");
+    } else {
       setCurrentQuestion(currentQuestion - 1);
     }
     MainButtonContentUpdate();
   }
 
-  // setting answered not or done 
-  function setQuestionFunction(value){
-    setQuestion( (question)=> 
-      question.map((q, i) => 
-       i == currentQuestion? { ...q, check:value}: q
-     )
-   );
-   return console.log(value)
+  // setting answered done or not done
+  function setQuestionFunction(value) {
+    setQuestion((question) =>
+      question.map((q, i) =>
+        i == currentQuestion ? { ...q, check: value } : q
+      )
+    );
   }
 
-  // Answer checking 
-  
-  async function CheckAnswer(index){
-    if (question[currentQuestion].check==1 || question[currentQuestion].check==2 ) {
-      alert("Already Answered")
+  // Set check 0 for all
+  function setQuestionFunctionAll(value) {
+    setQuestion((question) => question.map((q) => ({ ...q, check: value })));
+  }
+
+  // Answer checking
+  async function CheckAnswer(index) {
+    if (
+      question[currentQuestion].check == 1 ||
+      question[currentQuestion].check == 2
+    ) {
+      alert("Already Answered");
+    } else {
+      if (question[currentQuestion].correct == index) {
+        setQuestionFunction(2);
+        SetStatusBar("Correct");
+        SetnoOfQuestionAnswered(NoOfQuestionAnswered + 1);
+        SetnoOfQuestionCorrect(NoOfQuestionCorrect + 1);
+      } else {
+        setQuestionFunction(1);
+        SetStatusBar("Incorrect");
+        SetnoOfQuestionAnswered(NoOfQuestionAnswered + 1);
+        SetnoOfQuestionInCorrect(NoOfQuestionInCorrect + 1);
+      }
     }
-    else{
-      if (question[currentQuestion].correct==index) { 
-      setQuestionFunction(2)
-      SetStatusBar("Correct")
-      SetnoOfQuestionAnswered(NoOfQuestionAnswered+1)
-      SetnoOfQuestionCorrect(NoOfQuestionCorrect+1)
-    }
-    else{
-      setQuestionFunction(1)
-      SetStatusBar("Incorrect")
-      SetnoOfQuestionAnswered(NoOfQuestionAnswered+1)
-      SetnoOfQuestionInCorrect(NoOfQuestionInCorrect+1)
-    }}}
+  }
 
   return (
     <div className="mainArea">
       <div className="QuizArea">
-      <GenerateQuiz
-        mainQuestion={question[currentQuestion].mainQuestion}
-        options={question[currentQuestion].options}
-      ></GenerateQuiz>
-      <div className="buttonControls">
-        <button className="statusButton" onClick={PreviousButtonHandeller}>
-          Previous
-        </button>
-        <button className="statusButton" onClick={StartButtonHandeller}>
-          {mainButtonContent.content}
-        </button>
-        <button className="statusButton" onClick={NextButtonHandeller}>
-          Next
-        </button>
+        <GenerateQuiz
+          mainQuestion={question[currentQuestion].mainQuestion}
+          options={question[currentQuestion].options}
+        ></GenerateQuiz>
+        <div className="buttonControls">
+          <button className="statusButton" onClick={PreviousButtonHandeller}>
+            Previous
+          </button>
+          <button className="statusButton" onClick={StartButtonHandeller}>
+            {mainButtonContent.content}
+          </button>
+          <button className="statusButton" onClick={NextButtonHandeller}>
+            Next
+          </button>
+        </div>
+      </div>
+
+      <div className="scoreArea">
+        <h2 className="ScoreElements">Total Questions: {question.length}</h2>
+        <h2 className="ScoreElements">Answerd: {NoOfQuestionAnswered}</h2>
+        <h2 className="ScoreElements">Correct: {NoOfQuestionCorrect}</h2>
+        <h2 className="ScoreElements">Incorrect: {NoOfQuestionInCorrect}</h2>
+        <div className="statusBar">{StatusBar}</div>
       </div>
     </div>
-
-    <div className="scoreArea">
-      <h2 className="ScoreElements">Total Questions: {question.length}</h2>
-      <h2 className="ScoreElements">Answerd: {NoOfQuestionAnswered}</h2>
-      <h2 className="ScoreElements">Correct: {NoOfQuestionCorrect}</h2>
-      <h2 className="ScoreElements">Incorrect: {NoOfQuestionInCorrect}</h2>
-      <div className="statusBar">{StatusBar}</div>
-  
-    </div>
-
-
-    </div>
-    
   );
 
   function GenerateQuiz(props) {
@@ -221,9 +224,13 @@ function App() {
           <ul className="optionList">
             {props.options.map((option, index) => {
               return (
-                <li className="optionElements" key={index} onClick={() => {
-                  CheckAnswer(index)
-                }}>
+                <li
+                  className="optionElements"
+                  key={index}
+                  onClick={() => {
+                    CheckAnswer(index);
+                  }}
+                >
                   {option}
                 </li>
               );
